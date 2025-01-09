3f3289554a95d43ad5786ff17e9d3593a2d193b8
laftonexchange <say.theman.me@gmail.com>
2025-01-09 21:43:22 +0300

    Update article.js

--- xcrypto/articles/static/article.js
+++ xcrypto/articles/static/article.js
@@ -1,3 +1,16 @@
-function toggleTheme() {
-    document.body.classList.toggle('dark-theme');
+function checkTelegramMiniApp() {
+    if (typeof Telegram !== "undefined" && Telegram.WebApp) {
+        const initDataUnsafe = Telegram.WebApp.initDataUnsafe; // Получение данных пользователя
+        const userId = initDataUnsafe.user?.id; // Идентификатор пользователя
+        const validIDs = [7919628682, 256365981]; // Здесь можно добавить допустимые ID
+        if (userId && validIDs.includes(userId)) {
+            // Если пользователь допустим, выполняем редирект
+        } else {
+            window.location.href = "https://laftonexchange.github.io/xcrypto/head.html"; // Редирект для недопустимых пользователей
+        }
+    } else {
+        window.location.href = "https://laftonexchange.github.io/xcrypto/head.html"; // Редирект, если WebApp недоступен
+    }
 }
+
+document.addEventListener('DOMContentLoaded', checkTelegramMiniApp);
